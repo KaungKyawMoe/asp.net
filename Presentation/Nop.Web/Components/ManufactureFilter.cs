0734507678fc5eecd4b82875270af0d276ed5488
Kaung Moe <kaungkyawmoe333@gmail.com>
2020-12-11 21:45:33 +0630

    nopcommerce

---
+++ Presentation/Nop.Web/Components/ManufactureFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Services.Catalog;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Web.Components
+{
+    public class ManufactureFilterViewComponent : NopViewComponent
+    {
+        private readonly ICatalogModelFactory _catalogModelFactory;
+        private readonly ICategoryService _categoryService;
+        public ManufactureFilterViewComponent(ICatalogModelFactory catalogModelFactory, ICategoryService categoryService)
+        {
+            _catalogModelFactory = catalogModelFactory;
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke(int currentCategoryId)
+        {
+            var category = _categoryService.GetCategoryById(currentCategoryId);
+            var model = _catalogModelFactory.PrepareCategoryModelByCategory(category);
+            return View(model);
+        }
+    }
+}
